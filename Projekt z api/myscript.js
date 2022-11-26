262ed726f8ea1ce9feb8d1cba7f0d0d98a49651c
DomicjanJ <66036542+domicjanoss@users.noreply.github.com>
2022-11-26 21:28:52 +0100

    Dodanie styli, wyszukwiania,ikon i lekka zmiana html

--- Projekt z api/myscript.js
+++ Projekt z api/myscript.js
@@ -2,9 +2,16 @@ const API_KEY = 'api_key=d98d561ee867c2b1552a3783e05a9d32';
 const MAIN_URL = "https://api.themoviedb.org/3";
 const POPULAR = MAIN_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+<<<<<<< Updated upstream
 
 const main = document.getElementById('main');
 
+=======
+const search_URL = MAIN_URL + '/search/movie?' + API_KEY;
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search_bar');
+>>>>>>> Stashed changes
 
 
 // Popularne
@@ -28,4 +35,20 @@ function showMovies(data) {
     main.appendChild(movies);
     })
 }
+<<<<<<< Updated upstream
 getMovies(POPULAR);
+=======
+getMovies(POPULAR);
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const searchForm = search.value;
+    if (searchForm) {
+        getMovies(search_URL+'&query='+searchForm)
+    }
+    else{
+        showMovies(POPULAR)
+    }
+})
+>>>>>>> Stashed changes
